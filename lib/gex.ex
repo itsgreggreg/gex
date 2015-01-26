6c3043fe1ef6a92d641362088aeacf320fabb613
greggreg <greggreg@itsgreggreg.com>
2015-01-26 04:38:05 -0800

    init the .gex tree

--- lib/gex.ex
+++ lib/gex.ex
@@ -9,10 +9,45 @@ defmodule Gex do
   def init(opts \\ []) do
     unless gex_path do
       File.mkdir @gex_dir
+      # A keyword list that gets converted into a tree
+      # of directories and files needed to init a gex repo.
+      gex_init_tree = [
+        HEAD: "ref: refs/heads/master\n",
+        # If `--bare` was passed, write to the Git config indicating
+        # that the repository is bare. If `--bare` was not passed,
+        # write to the Git config saying the repository is not bare.
+        # config: GexConfig.to_string([
+        #            core: [ "": [ bare: opts.bare === true ]]
+        #         ]),
+        objects: [],
+        refs: [
+          heads: [],
+        ]
+      ]
+      write_tree_to_gex_dir(gex_init_tree)
     else
       IO.puts "Already in a gex repository."
     end
   end
+
+  # Takes a tree describing directories and files and
+  # writes those dirs and files to the gex dir.
+  defp write_tree_to_gex_dir(tree, path \\ gex_path) do
+    for key <- Keyword.keys(tree) do
+      path = Path.join(path, Atom.to_string(key))
+      case tree[key] do
+        # If it is a string, write it as a file
+        file when is_binary(file) ->
+          File.write!(path, file)
+        # If it is a list, make the dir and
+        # recurse into it
+        tree when is_list(tree) ->
+          unless File.exists?(path), do: File.mkdir(path)
+          write_tree_to_gex_dir(tree, path)
+      end
+    end
+  end
+
 
   # Serches up the directory tree for a @gex_dir
   # Returns the path if found
@@ -27,4 +62,13 @@ defmodule Gex do
     end
   end
 
+end
+
+
+defmodule GexConfig do
+  defstruct core: []
+
+  def to_string do
+
+  end
 end
