12f93e0eb57d93cf793ed25aa5b07573bb18b7bc
greggreg <greggreg@itsgreggreg.com>
2015-01-26 05:28:46 -0800

    supports creation of bare repos

--- lib/gex.ex
+++ lib/gex.ex
@@ -1,5 +1,24 @@
-defmodule Gex do
+defmodule GexConfig do
+  defstruct core: []
 
+  def to_string(conf) do
+    if core = conf.core do
+       "[core]\n" <> for {key, val} <- core, into: "" do
+         "  #{key} = #{val}\n"
+       end
+    end
+  end
+
+  def valid_file?(path) do
+    case File.read(path) do
+      {:ok, contents} -> Regex.match?(~r/\[core\]/, contents)
+      _               -> false
+    end
+  end
+end
+
+defmodule Gex do
+  import GexConfig
   # folder to store repo information
   @gex_dir ".gex"
   # system dependent root directory
@@ -8,7 +27,8 @@ defmodule Gex do
   @doc "Initializes an empty repository"
   def init(opts \\ []) do
     unless gex_path do
-      File.mkdir @gex_dir
+      unless opts[:bare], do: File.mkdir @gex_dir
+      if opts[:bare], do: File.write("config", "[core]")
       # A keyword list that gets converted into a tree
       # of directories and files needed to init a gex repo.
       gex_init_tree = [
@@ -16,9 +36,9 @@ defmodule Gex do
         # If `--bare` was passed, write to the Git config indicating
         # that the repository is bare. If `--bare` was not passed,
         # write to the Git config saying the repository is not bare.
-        # config: GexConfig.to_string([
-        #            core: [ "": [ bare: opts.bare === true ]]
-        #         ]),
+        config: GexConfig.to_string(%GexConfig{
+                   :core => [ bare: opts[:bare] ]
+                }),
         objects: [],
         refs: [
           heads: [],
@@ -50,25 +70,20 @@ defmodule Gex do
 
 
   # Serches up the directory tree for a @gex_dir
+  # or a file named 'config' that contains '[core]'
   # Returns the path if found
   # Returns nil if not found
   defp gex_path(path \\ File.cwd!) do
-    if File.dir?( gp = Path.join(path, @gex_dir)) do
-      gp
-    else
-      unless path == @system_root do
-        path |> Path.dirname |> gex_path
-      end
+    gex_dir     = Path.join(path, @gex_dir)
+    config_file = Path.join(path, "config")
+    cond do
+      File.dir? gex_dir -> gex_dir
+      GexConfig.valid_file? config_file -> path
+      path == @system_root -> nil
+      true -> path |> Path.dirname |> gex_path
     end
   end
 
 end
 
 
-defmodule GexConfig do
-  defstruct core: []
-
-  def to_string do
-
-  end
-end
